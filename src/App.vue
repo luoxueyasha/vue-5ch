a6f4c771c5ca23b092654790a211cf1783e78556
luoxueyasha <jyaseijikin@163.com>
2020-12-30 17:52:25 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <THREADCONTENTS />
+  </div>
+</template>
+
+<script>
+import THREADCONTENTS from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    THREADCONTENTS
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family:Saitamaar,"MS Gothic",Simsun;
+  margin: 0;
+  padding: 0;
+  color: #000000;
+  background: #ffffff;
+  background-repeat: repeat;
+  background-attachment: fixed;
+  font-weight: 600;
+
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+//use "npm run serve" for running
+
+import Vue from 'vue'
+import App from './App.vue'
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
