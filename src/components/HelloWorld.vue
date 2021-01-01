995099ad6582d877745ef26f47c14f660c3577d2
luoxueyasha <bing1273103582@outlook.com>
2021-01-01 18:18:39 +0800

    push

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="THREADCONTENTS" id="THREADCONTENTS">
     <h3 class="thread_title">
       <!--标题 start-->
-      <span>标题 </span>
+      <span>{{title}}</span>
       <!--标题 end-->
     </h3>
     <br />
@@ -41,7 +41,7 @@
     <textarea
       class="iden"
       v-model="threadname"
-      placeholder="读取/储存内容"
+      placeholder="读取/储存名"
     ></textarea>
     <button @click="savethread">导出帖子</button>
     <button @click="loadthread">导入帖子</button>
@@ -54,6 +54,8 @@ export default {
   data() {
     return {
       list: [],
+      title:"标题",
+//
       name: "",
       iden: "",
       txt: "",
@@ -109,129 +111,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-div.THREADCONTENTS {
-  z-index: 1;
-  width: 93%;
-  margin: 0.5em auto 0.75em auto;
-  padding: 0.75em;
-  border: 1px solid #333;
-  color: #333;
-  border-radius: 0.75em / 0.75em;
-  background: #efefef;
-  font-size: 1em;
-}
-
-a {
-  border-width: 0px;
-}
-
-a:link {
-  background: transparent;
-  color: #0000cc;
-}
-
-a:active {
-  background: transparent;
-  color: #0000cc;
-}
-
-a:visited {
-  background: transparent;
-  color: #0000cc;
-}
-
-h3 {
-  margin: 0;
-  padding: 0.5em 0;
-  text-align: left;
-  font-size: 1em;
-}
-
-h3 span.common {
-  padding: 0.5em;
-  border-radius: 0.5em / 0.5em;
-  background: #39f;
-  color: #fff;
-  font-size: 1em;
-}
-
-h3 span {
-  padding: 0 0 0.5em 1em;
-  color: #ff0000;
-  font-size: 1.5em;
-}
-
-h3.thread_title {
-  font-family: Saitamaar, "MS Gothic";
-  margin: 0;
-  padding: 0 2em 0 3em;
-  text-indent: -4em;
-}
-
-div.board_header {
-  margin: 0 1em 1em 1em;
-  padding: 0.5em;
-  color: #333;
-  background: #efe;
-  border-radius: 0.25em / 0.25em;
-  font-size: 1em;
-  height: 15em;
-  overflow-y: scroll;
-}
-
-div.keyword {
-  margin: 0;
-  padding: 0;
-  color: #333;
-  background: inherit;
-  font-size: 1em;
-}
-
-dt {
-  margin: 0px 0.5em;
-  padding: 0px;
-  font-size: 1em;
-}
-
-dd {
-  margin-bottom: 2em;
-  font-size: 1em;
-  font-weight: normal;
-  white-space: pre-line;
-}
-
-span.keyword0 {
-  padding: 0.5em;
-  border-radius: 0.5em / 0.5em;
-  background: #39f;
-  color: #fff;
-  font-size: 0.75em;
-  line-height: 2.5em;
-  word-break: keep-all;
-}
-
-span.keyword1 {
-  padding: 0.5em;
-  border-radius: 0.5em / 0.5em;
-  background: #f63;
-  color: #fff;
-  font-size: 0.75em;
-  line-height: 2.5em;
-  word-break: keep-all;
-}
-
-textarea.iden {
-  font-family: Saitamaar, "MS Gothic", Simsun;
-  white-space: pre-line;
-  width: 30%;
-  height: 10%;
-}
-
-textarea.txt {
-  font-family: Saitamaar, "MS Gothic", Simsun;
-  width: 80%;
-  height: 60%;
-  min-height: 200px;
-  white-space: pre-line;
-}
+@import "./style.css";
 </style>
